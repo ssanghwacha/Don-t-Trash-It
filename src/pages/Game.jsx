39dc22407b04a0b53e76ddd5195dfafb4518f44f
Sangwha Cha <44427215+ssanghwacha@users.noreply.github.com>
2025-04-09 16:14:22 -0700

    1

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import './Game.css';
 import bg from '../assets/main-bg.png';
 
-const MainPage = () => {
+const Game = ({ onThrow, onPick }) => {
     return (
-        <div className="main-container">
-            <img src={bg} alt="Background" className="background-image" />
+        <div className="game-container">
+            <img src={bg} alt="background" className="game-bg" />
+            {/* 여기에 나중에 버튼이나 트리 등을 추가하면 돼 */}
         </div>
     );
 };
 
-export default MainPage;
+export default Game;
